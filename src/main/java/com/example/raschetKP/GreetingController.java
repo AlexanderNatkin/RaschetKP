6e8c4c4cfc1e6ef9f75d5a060ae41439b82ac616
Alexander Natkin <natkin163@gmail.com>
2021-07-14 08:27:15 +0400

    Добавил testPage, затестил GetMapping

--- src/main/java/com/example/raschetKP/GreetingController.java
+++ src/main/java/com/example/raschetKP/GreetingController.java
@@ -3,14 +3,13 @@ package com.example.raschetKP;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.Map;
 
 @Controller
 public class GreetingController {
-
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
+                                       String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
@@ -18,5 +17,10 @@ public class GreetingController {
     public String main (Map<String, Object> model) {
         model.put("some", "Hello, Bitches!");
         return "main";
+    }
+    @GetMapping("/testPage")
+    public String testPage (Map<String, Object> model) {
+        model.put("sameOne", "Hi, I am very happy!");
+        return "testPage";
     }
 }
